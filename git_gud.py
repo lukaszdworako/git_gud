ba2244c6035a2686ed0eeba03877be8f1927b17b
Lukasz Dworakowski <lukasz.dworakowski@mail.utoronto.com>
2017-11-30 23:54:40 -0500

    Git pull rekt gud

---
+++ git_gud.py
@@ -0,0 +1,9 @@
+def git_gud():
+    temp = input("Did you do gud? (yes/ no)\n")
+    if temp == "yes":
+        print("DW brah, you good fam, you good")
+    elif temp == "no":
+        temp_1 = int(input("Bruh, why you not good, how bad are you numerically?\n"))
+        i = 0
+        for i in range(temp_1):
+            print("Bruh, you need to git gud\n")
