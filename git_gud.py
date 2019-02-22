1fee2b57af5eb724a9874ec8745744d21aabc221
Lukasz Dworakowski <lukasz.dworakowski@mail.utoronto.ca>
2019-02-21 20:19:47 -0500

    Update git_gud.py
    remove my stupidity

--- git_gud.py
+++ git_gud.py
@@ -4,6 +4,5 @@ def git_gud():
         print("DW brah, you good fam, you good")
     elif temp == "no":
         temp_1 = int(input("Bruh, why you not good, how bad are you numerically?\n"))
-        i = 0
         for i in range(temp_1):
             print("Bruh, you need to git gud\n")
