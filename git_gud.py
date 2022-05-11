edb93b843e68be50a1967803da16f8c4349d3b4b
Lukasz Dworakowski <lukasz.dworakowski@mail.utoronto.ca>
2022-05-10 22:04:55 -0400

    update to meet more of the current standards. Address potential security issues

--- git_gud.py
+++ git_gud.py
@@ -1,8 +1,18 @@
 def git_gud():
-    temp = input("Did you do gud? (yes/ no)\n")
-    if temp == "yes":
+    didGudIn = ""
+    while ((not didGudIn.isalpha()) or (didGudIn.lower() not in ["yes", "no"])):
+        didGudIn = input("Did you do gud? (yes/ no): ")
+    
+    if (didGudIn.lower() == "yes"):
         print("DW brah, you good fam, you good")
-    elif temp == "no":
-        temp_1 = int(input("Bruh, why you not good, how bad are you numerically?\n"))
-        for i in range(temp_1):
-            print("Bruh, you need to git gud\n")
+    else:
+        numericBad = ""
+        while(not numericBad.isdecimal()):
+            numericBad = input("Bruh, why you not good, how bad are you numerically: ")
+        numericBad = int(numericBad)
+        for i in range(numericBad):
+            print("Bruh, you need to git gud")
+    
+
+if __name__ == "__main__":
+    git_gud()
